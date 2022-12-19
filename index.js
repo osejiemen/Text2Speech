090003ab3a5f74bcf1c70cdc87f98c458df02542
osejiemen <osejiemen@gmail.com>
2022-12-19 13:05:36 +0100

    created and styled project with html and css

---
+++ index.js
@@ -0,0 +1,10 @@
+
+prompt("ose's arrival");
+
+  var days = Math.floor(Math.random()*10) ;
+
+if(days < 5){
+  alert(`welcome ose this is ${days} days`)
+} else{
+  alert(`you're late this is ${days} days` )
+};
